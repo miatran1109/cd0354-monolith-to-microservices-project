129b47a792ce0608cbae60348c499b9ea6824d96
MaiTran <miatran1109@gmail.com>
2023-11-20 07:09:30 +0700

    change env

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad77227e467574dbba146b31eb83bd8d-887541982.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
